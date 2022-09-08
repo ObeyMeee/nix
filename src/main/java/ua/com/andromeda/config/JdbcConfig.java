c8a5ce12455a7857d9d3164e896541b6b72c857f
Andrrr <geraskin19970309@gmail.com>
2022-09-08 11:29:31 +0300

    update database data for jdbc

--- src/main/java/ua/com/andromeda/config/JdbcConfig.java
+++ src/main/java/ua/com/andromeda/config/JdbcConfig.java
@@ -8,9 +8,9 @@ import java.sql.DriverManager;
 
 @Singleton
 public class JdbcConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/test_jdbc";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1337";
+    private static final String URL = "jdbc:postgresql://ec2-18-208-55-135.compute-1.amazonaws.com/dfn7rh7jm4g1v9";
+    private static final String USERNAME = "beuphqsiyszphy";
+    private static final String PASSWORD = "b40be8cd73627658411c1e726abaef3a140f0e97bf043463d9d88baff83dbfac";
     private final Connection connection;
 
     @SneakyThrows
